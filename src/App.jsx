a0875e8ac38efc5c09d2b674ce0f6a18315379d3
AAYUSHI1507 <a.kataria@somaiya.edu>
2024-11-27 03:48:31 +0530

    Keeping consistency with one commit and making small chage doing extra things also

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Navbar from './components/Navbar'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Navbar from "./components/Navbar";
 //import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  // States
+  const [todo, setTodo] = useState(""); // This is the input text user is providing.
+  const [Todo, setTodos] = useState([]); // This is to hold the array of todo.
+  //Functions
+  const HandleEdit = () => {};
+  const HandleDelete = () => {};
+  const handleAdd = () => {};
   return (
     <>
-    <Navbar />
-    <div className="container mx-auto bg-red-100 rounded-xl my-4 p-3 min-h-[80vh]">
-      <div className="addTodo my-4">
-        <h2 className="text-lg font-bold">Add a Todo</h2>
-        <input type="text" className='w-1/2 h-8 rounded-md'/>
-        <button className='bg-red-500 hover:bg-red-700 font-bold mx-2 my-1 text-red-200 px-2 py-1 rounded-lg'>Add</button>
-      </div>
-        <h2 className='text-lg font-bold'>Yours Todos</h2>
+      <Navbar />
+      <div className="container mx-auto bg-red-100 rounded-xl my-4 p-3 min-h-[80vh]">
+        <div className="addTodo my-4">
+          <h2 className="text-lg font-bold">Add a Todo</h2>
+          <input type="text" className="w-1/2 h-8 rounded-md" />
+          <button
+            className="bg-red-500 hover:bg-red-700 font-bold mx-2 my-1 text-red-200 px-2 py-1 rounded-lg"
+            onClick={handleAdd}
+          >
+            Add
+          </button>
+        </div>
+        <h2 className="text-lg font-bold">Yours Todos</h2>
         <div className="todos">
           <div className="todo flex">
-            <div className="text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</div>
+            <div className="text">
+              {todo}
+            </div>
             <div className="buttons">
-              <button className='bg-red-500 hover:bg-red-700 font-bold mx-3 -my-1 text-red-200 px-2 py-1 rounded-lg'>Edit</button>
-              <button className='bg-red-500 hover:bg-red-700 font-bold mx-1 -my-1 text-red-200 px-2 py-1 rounded-lg'>Delete</button>
+              <button
+                className="bg-red-500 hover:bg-red-700 font-bold mx-3 -my-1 text-red-200 px-2 py-1 rounded-lg"
+                onClick={HandleEdit}
+              >
+                Edit
+              </button>
+              <button
+                className="bg-red-500 hover:bg-red-700 font-bold mx-1 -my-1 text-red-200 px-2 py-1 rounded-lg"
+                onClick={HandleDelete}
+              >
+                Delete
+              </button>
             </div>
           </div>
         </div>
-    </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
